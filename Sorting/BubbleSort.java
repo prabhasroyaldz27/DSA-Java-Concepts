c178e5555730094545ffbc8b9b84e7a005e22d0e
Prabhas Royal Vusa <158466732+prabhasroyaldz27@users.noreply.github.com>
2025-08-03 18:59:43 +0530

    Create BubbleSort.java
    Bubble Sort : It is a sorting technique in which the maximum/minimum element is pushed to the end of the array by swapping with the adjacent element.
    
    Stable Sort : Bubble sort is a stable sort, meaning if two elements have the same key value, then after sorting their relative positions won’t change.
    
    Time Complexity = O(N²)
    Space Complexity = O(1) Auxiliary Space

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Solution {
+    public static void bubbleSort(int arr[],int n){
+           for(int i=0;i<n-1;i++){
+              for(int j=0;j<n-i-1;j++){
+                  if(arr[j]>arr[j+1]){
+                     //Swap Elements
+                     int temp=arr[j];
+                     arr[j]=arr[j+1];
+                     arr[j+1]=temp;
+                  }
+              }
+           }
+    }
+    public static void printArray(int arr[],int n){
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Original Array : ");
+        printArray(arr,n);
+        bubbleSort(arr,n);
+        System.out.print("Sorted Array : ");
+        printArray(arr,n);
+    }
+}
